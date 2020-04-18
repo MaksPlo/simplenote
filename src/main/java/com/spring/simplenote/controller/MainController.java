78c13d401963736162011abd366d4c89c1d9ed76
MaksimPloskodnyak <maksim_ploskodniak@epam.com>
2020-04-18 04:06:25 +0300

    Added webpack. Added front components.

--- src/main/java/com/spring/simplenote/controller/MainController.java
+++ src/main/java/com/spring/simplenote/controller/MainController.java
@@ -3,6 +3,7 @@ package com.spring.simplenote.controller;
 import com.spring.simplenote.model.User;
 import com.spring.simplenote.repository.NoteRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,6 +19,9 @@ import java.util.Map;
 public class MainController {
     private final NoteRepository noteRepository;
 
+    @Value("${spring.profiles.active}")
+    private String profile;
+
     @GetMapping
     public String main(Model model, @AuthenticationPrincipal User user) {
         Map<Object, Object> data = new HashMap<>();
@@ -26,6 +30,7 @@ public class MainController {
         data.put("notes", noteRepository.findAll());
 
         model.addAttribute("frontendData", data);
+        model.addAttribute("isDevMode", "dev".equals(profile));
 
         return "index";
     }
---
+++ src/main/resources/static/js/pages/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div v-if="!profile">Необходимо авторизоваться через
+            <a href="/login">Google</a>
+        </div>
+        <div v-else>
+            <div>{{profile.name}}&nbsp;<a href="/logout">Выйти</a></div>
+            <notes-list :notes="notes" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import NotesList from '../components/notes/NoteList.vue'
+
+    export default {
+        components:{
+            NotesList
+        },
+        data() {
+            return {
+                notes: frontendData.notes,
+                profile: frontendData.profile
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
